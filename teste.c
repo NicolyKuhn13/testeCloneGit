c48b6a1af17730cbf2db4a093200d995588d8a5f
Nicoly <nicolycolorsk@gmail.com>
2020-02-14 08:21:29 -0300

    cod q o prof pediu

--- teste.c
+++ teste.c
@@ -3,6 +3,14 @@
 
 int main(){
 
+    int idade;
+
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+
+    idade = idade - 1;
+
+    printf("Sua idade menos 1: %d\n", idade);
 
     return 0;
 }
